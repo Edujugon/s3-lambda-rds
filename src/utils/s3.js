9557891a61cac103d7605a4c9b4281194a150a3f
Eduardo Marcos <edujugon@gmail.com>
2020-07-28 17:46:12 +0200

    make it more readable.

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -5,10 +5,12 @@ const s3 = new AWS.S3();
 const BUCKET_NAME = process.env.BUCKET_NAME;
 
 const getObject = key => {
-  s3.getObject({
+  const params = {
     Bucket: BUCKET_NAME,
-    Key: key,
-  }).promise();
+    Key: key
+  };
+  
+  s3.getObject(params).promise();
 }
 
 module.exports = {
