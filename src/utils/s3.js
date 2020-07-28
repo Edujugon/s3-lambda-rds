6546ef1051712052f8121873f5e26f0500f0d410
Eduardo Marcos <edujugon@gmail.com>
2020-07-28 17:37:45 +0200

    make variable name consistent

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -4,10 +4,10 @@ const s3 = new AWS.S3();
 
 const BUCKET_NAME = process.env.BUCKET_NAME;
 
-const getObject = name => {
+const getObject = key => {
   s3.getObject({
     Bucket: BUCKET_NAME,
-    Key: name,
+    Key: key,
   }).promise();
 }
 
